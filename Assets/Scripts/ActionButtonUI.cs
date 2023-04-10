1fa321550eb809394c2fd4ce15842d2b701bf5fb
Pithak-00 <31059244+Pithak-00@users.noreply.github.com>
2023-04-10 16:35:09 +0900

    SelectedActionUIVisual

--- Assets/Scripts/ActionButtonUI.cs
+++ Assets/Scripts/ActionButtonUI.cs
@@ -8,14 +8,24 @@ public class ActionButtonUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textMeshPro;
     [SerializeField] private Button button;
+    [SerializeField] private GameObject selectedGameObject;
+
+    private BaseAction BaseAction;
 
     public void SetBaseAction(BaseAction baseAction)
     {
+        this.BaseAction = baseAction;
         textMeshPro.text = baseAction.GetActionName().ToUpper();
 
         button.onClick.AddListener(() =>
         {
             UnitActionSystem.Instance.SetSelectedAction(baseAction);
         });
+    }
+
+    public void UpdateSelectedVisual()
+    {
+        BaseAction selectedBaseAction = UnitActionSystem.Instance.GetSelectedAction();
+        selectedGameObject.SetActive(selectedBaseAction == BaseAction);
     }
 }
