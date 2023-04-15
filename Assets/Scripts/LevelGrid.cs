889faa497d18cb9191005a8e08040b325878eb6c
Pithak-00 <31059244+Pithak-00@users.noreply.github.com>
2023-04-15 19:26:59 +0900

    GridSystemGenerics

--- Assets/Scripts/LevelGrid.cs
+++ Assets/Scripts/LevelGrid.cs
@@ -11,7 +11,7 @@ public class LevelGrid : MonoBehaviour
 
     [SerializeField] private Transform gridDebugObjectPrefab;
 
-    private GridSystem gridSystem;
+    private GridSystem<GridObject> gridSystem;
 
     private void Awake()
     {
@@ -22,7 +22,8 @@ public class LevelGrid : MonoBehaviour
         }
         Instance = this;
 
-        gridSystem = new GridSystem(10, 10, 2f);
+        gridSystem = new GridSystem<GridObject>(10, 10, 2f,
+            (GridSystem<GridObject>g,GridPosition gridPosition) => new GridObject(g,gridPosition));
         gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
     }
 
